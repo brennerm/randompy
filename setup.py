da57f316e00ac94ffbb7e3d9be26031d5fae34d4
brennerm <xamrennerb@gmail.com>
2017-03-13 20:32:29 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pyrandom',
+    packages=['pyrandom'],
+    version='1.0',
+    description='Collection of functions to generate pseudo random values for emails, IP addresses, ...',
+    author='Max Brenner',
+    author_email='xamrennerb@gmail.com',
+    url='https://github.com/brennerm/pyrandom',
+    download_url='https://github.com/brennerm/pyrandom/archive/1.0.tar.gz',
+    keywords=['random', 'generator', 'ipv4', 'ipv6', 'string', 'integer', 'datetime', 'mail', 'mac', 'array']
+)
